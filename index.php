1aca4437215bc1e762317d640ffa33143661abb4
lyh71709 <60583161+lyh71709@users.noreply.github.com>
2020-07-29 13:21:30 +1200

    Checkpoint 1

--- index.php
+++ index.php
@@ -1,42 +1,6 @@
-<!DOCTYPE HTML>
-
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="description" content="Put Content Here">
-    <meta name="keywords" content="Put keywords here">
-    <meta name="author" content="Put your name here">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <title>Website Title goes here</title>
-    
-    <!-- Edit the link below / replace with your chosen google font -->
-    <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet"> 
-    
-    <!-- Edit the name of your style sheet - 'foo' is not a valid name!! -->
-    <link rel="stylesheet" href="css/no_side.css"> 
-    
-</head>
-    
-<body>
-    
-    <div class="wrapper">
-    
-
-        
-        <div class="box banner">
-            
-        <!-- logo image linking to home page goes here -->
-        <a href="index.php">
-            <div class="box logo"  title="Logo - Click here to go to the Home Page">
-            <img class="img-circle" src="images/gen_logo.png" width="150" height="150" alt="generic logo" />
-            
-            </div>    <!-- / logo -->
-        </a>
-            
-            <h1>Website Name</h1>
-        </div>    <!-- / banner -->
+<?php
+    include "topbit.php";
+?>
         
         <div class="box main">
             <h2>First Page heading</h2>
@@ -54,12 +18,7 @@
             </p>
             
         </div>    <!-- / main -->
-        
 
-        <div class="box footer">
-            CC yourname 20XX
-        </div>    <!-- / footer -->
-    
-    </div>  <!-- / wrapper  -->
-    
-</body>        
+<?php
+    include "bottombit.php";
+?>
